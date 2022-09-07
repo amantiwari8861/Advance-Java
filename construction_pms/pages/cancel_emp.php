357a9ca9a6669f97e319737247410d20312e59f3
Aman Tiwari <36691571+amantiwari8861@users.noreply.github.com>
2022-09-07 23:57:17 +0530

    removed php

--- construction_pms/pages/cancel_emp.php
+++
@@ -1,8 +0,0 @@
-<?php
-include '../includes/db.php';
-
-$query= mysqli_query($conn,"DELETE from employee where eid = '".$_GET['id']."'");
-if($query){
-	header("location:index.php?page=employee&io=1");
-}
-?>
--- construction_pms/pages/cancel_proj.php
+++
@@ -1,8 +0,0 @@
-<?php
-include '../includes/db.php';
-
-$query= mysqli_query($conn,"DELETE from projects where project_id = '".$_GET['id']."'");
-if($query){
-	header("location:fggs.php?page=project_list&io=1");
-}
-?>
